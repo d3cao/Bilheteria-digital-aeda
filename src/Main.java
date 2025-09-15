e9e62714c2b41e2a15b011402a5ec874ec54644d
João Pedro Severo <142758081+JpSeveroo@users.noreply.github.com>
2025-09-15 19:13:40 -0300

    tentaiva de interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String continuar;
+
+        do {
+            System.out.println("----- ATENDIMENTO AO CLIENTE -----");
+            System.out.print("Digite a quantidade de ingressos: ");
+            int quantidadeIngressos = scanner.nextInt();
+
+            System.out.print("TIPOS DE INGRESSOS");
+            System.out.println("[1] - Inteira");
+            System.out.println("[2] - Meia");
+            System.out.println("[3] - Promocional");
+            System.out.print("Escolha o tipo de ingresso (1, 2 ou 3): ");
+            int tipoIngresso = scanner.nextInt();
+
+            System.out.print("Digite a idade do cliente: ");
+            int idadeCliente = scanner.nextInt();
+
+            System.out.print("\nAtender próximo cliente? (S/N): ");
+            scanner.nextLine();
+            continuar = scanner.nextLine().trim().toLowerCase();
+
+        } while(continuar.equals("s"));
+        System.out.println("Programa encerrado.");
+        scanner.close();
     }
 }
