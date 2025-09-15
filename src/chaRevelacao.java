577a42c74220f7ac1dee7ec968fb74c563a6ed42
João Pedro Severo <142758081+JpSeveroo@users.noreply.github.com>
2025-09-15 19:45:40 -0300

    update cha revelacao

---
+++ src/chaRevelacao.java
@@ -0,0 +1,6 @@
+public class chaRevelacao {
+    public static void main(String[] args) {
+        teste maiorgay = new teste("André Felipe", 18, "Chupador de bolas");
+        maiorgay.exibir();
+    }
+}
